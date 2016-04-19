f7855f077f577fd04c7ff0d962466813292b68e8
action781 <action781@gmail.com>
2016-04-18 23:47:55 -0400

    weekend work

--- server.R
+++ server.R
@@ -1,2 +1,43 @@
 #server.R
 
+library(ggplot2)
+library(dplyr)
+install.packages("DT")
+library(DT)
+WHO <- read.csv("Data/WHO.csv")
+
+shinyServer(function(input, output) {
+    
+    top10 <- reactive({
+       arrange(WHO, desc(input$var))[1:10,]
+       }) 
+
+      
+    output$plot <- renderPlot({
+        barplot(top10(), 
+                main=input$var,
+                ylab=input$var,
+                xlab="Country")    
+        })
+    
+    output$table <- DT::renderDataTable({
+        DT::datatable(top10())
+        
+    })
+    
+}
+)
+
+
+##top 10 pareto chart
+# person picks variable and my chart shows the top 10 countries for each
+# another tab shows table
+# 
+# 
+# varswitch <- switch(input$var, 
+#                     "Percent White" = counties$white,
+#                     "Percent Black" = counties$black,
+#                     "Percent Hispanic" = counties$hispanic,
+#                     "Percent Asian" = counties$asian)
+
+
--- ui.R
+++ ui.R
@@ -1,2 +1,30 @@
 #ui.R
 
+#WHO Database 2012
+
+library(shiny)
+
+shinyUI(fluidPage(
+    titlePanel("Literacy Rate Across the World by Region"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            # Define the sidebar with one input
+                selectInput("var", "Select the Variable:", 
+                            choices=colnames(WHO[3:13])
+                            )
+                ),
+    
+        mainPanel(
+            tabsetPanel(
+                tabPanel("plot", plotOutput("plot")),
+                tabPanel("table",
+                         DT::dataTableOutput("table")
+                )
+                )
+            )
+    )
+))
+
+
+
